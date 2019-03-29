3942f4263225e5abb83298e66370f08d780a06d7
HAMDAOUI Slimane <InfoDZ44@yahoo.com>
2019-03-29 18:55:12 +0100

    my second commit

---
+++ src/gestion_de_scolarité/PL/StudentAccount/LesAnnances/AnnanceModèl.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestion_de_scolarité.PL.StudentAccount.LesAnnances;
+
+import java.util.Date;
+
+/**
+ *
+ * @author slimane
+ */
+public class AnnanceModèl {
+    
+ private Date date;   
+ private String status;
+ private String categorie;
+ private String titre;
+ private String detail;
+
+    public AnnanceModèl(Date date, String status, String categorie, String titre, String detail) {
+        this.date = date;
+        this.status = status;
+        this.categorie = categorie;
+        this.titre = titre;
+        this.detail = detail;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
+}
